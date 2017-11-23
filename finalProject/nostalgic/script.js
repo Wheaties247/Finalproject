3dfdea4d6e2c0f7a9ad195dc5f6b30342fc66fb0
Wheaties247 <Timothylowe247@gmail.com>
2017-11-23 01:01:54 -0500

    Add files via upload
    ive beein having some issues with my parallax effect currently, if you know what could be the issue help a brother out.

---
+++ finalProject/nostalgic/script.js
@@ -0,0 +1,9 @@
+//Audio Function
+var backgroundMusic= new Audio('audio/instrumental.mp3');
+backgroundMusic.play();
+
+
+//FUNCTION TO ANIMATE NAV BAR drop down
+// function animateOnClick() {
+// 	document.getElementById('').className = '';
+// }
